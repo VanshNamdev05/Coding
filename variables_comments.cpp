c590f7a1d48d36baa5a76f9a62c0a64fdf3951cb
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-04-29 19:14:31 +0530

    Add files via upload

---
+++ variables_comments.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // comments:---
+    // irony hori hai ji irony :)
+    /*bhot
+    jaada
+    irony 
+    hori 
+    hai 
+    ji*/
+    int sum = 6;
+    cout <<"hello world"<<sum;
+    
+    return 0;
+}
