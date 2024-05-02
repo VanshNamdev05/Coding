93777de905be3cc260e67498dbe6b5221f73626e
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-05-02 19:12:55 +0530

    Add files via upload

---
+++ do_while_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 2;
+    do{
+        cout<<i;
+    }
+    while(i<1);
+    // do while me ek na ek baar execute hoga code
+    return 0;
+}
