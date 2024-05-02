df40cdd2e1ae9de96f61a98c337e0f3d2e03f0b4
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-05-02 19:11:31 +0530

    Add files via upload

---
+++ for_loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"enter the number : ";
+    cin>>n;
+    // printing 1 to n numbers : 
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
