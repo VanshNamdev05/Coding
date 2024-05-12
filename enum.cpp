bd387ff5bbfeaf4f104b047d56bae45466da64ba
VanshNamdev05 <159474384+VanshNamdev05@users.noreply.github.com>
2024-05-12 12:21:43 +0530

    Add files via upload

---
+++ enum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    enum meal{breakfast,lunch,dinner};//0,1,2,3,.... aise assign kr dega values sabko
+    cout<<breakfast<<endl;
+    cout<<lunch<<endl;
+    cout<<dinner<<endl;
+    return 0;
+}
